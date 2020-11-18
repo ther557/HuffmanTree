a11903ab3b0ee61209f8f06c17390eee3d937cf3
ther557 <74394492+ther557@users.noreply.github.com>
2020-11-18 12:08:21 +0800

    Update Project.h

--- include/Project.h
+++ include/Project.h
@@ -1,9 +1,10 @@
 #ifndef _PROJECT_H_
 #define _PROJECT_H_
 
-void FileRead(FIle *fp,char *buff);
+void FileRead(FIle *fp,char *buff[]);
 void BuildTree(char buff,HTNode *tree[]);
 void Code(HTNode tree[],char *HuffCode[],int n);
-void Uncode(char HuffCode,FILE *fp);
+void Uncode(char HuffCode[],FILE *fp);
+void WeightInput(char buff[],char *weight[]);
 
 #endif
