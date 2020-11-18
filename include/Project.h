669d471a6bb621d8f8d975d02e9c1230c184ea82
ther557 <74394492+ther557@users.noreply.github.com>
2020-11-18 11:37:45 +0800

    Create Project.h

---
+++ include/Project.h
@@ -0,0 +1,9 @@
+#ifndef _PROJECT_H_
+#define _PROJECT_H_
+
+void FileRead(FIle *fp,char *buff)
+void BuildTree(char buff,HTNode *tree[]);
+void Code(HTNode tree[],char *HuffCode[],int n);
+void Uncode(char HuffCode,FILE *fp);
+
+#endif
