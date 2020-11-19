8590a3c42cf31ed76c57b307a9c2f2a86df5af68
DevilInChina <905320627@qq.com>
2020-11-19 14:24:59 +0800

    finish some bug

--- include/Project.h
+++ include/Project.h
@@ -1,7 +1,10 @@
 #ifndef _PROJECT_H_
 #define _PROJECT_H_
+#include <stdio.h>
+typedef struct HTNode{
 
-void FileRead(FIle *fp,char *buff[]);
+}HTNode;
+void FileRead(const char *file,char *buff);
 void BuildTree(char buff,HTNode *tree[]);
 void Code(HTNode tree[],char *HuffCode[],int n);
 void Uncode(char HuffCode[],FILE *fp);
