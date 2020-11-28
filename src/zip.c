bf48f1318a7bdb3702b7ad38ac200c263ecae3c9
DevilInChina <905320627@qq.com>
2020-11-28 10:29:00 +0800

    optimize structure

---
+++ src/zip.c
@@ -0,0 +1,10 @@
+#include <Project.h>
+int main (int argc,char **argv){
+    if(argc!=3) {
+        // print error info
+        return 1;
+    }
+    char buff[256];///?
+    FileRead(argv[1],buff);
+
+}
